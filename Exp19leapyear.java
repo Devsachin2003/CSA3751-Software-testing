497b37474eb50fa76471e77b68e4a7b0089c8ab6
S.G.DEVSACHIN <112550872+Devsachin2003@users.noreply.github.com>
2023-10-23 11:17:03 +0530

    Create Exp19leapyear.java

---
+++ Exp19leapyear.java
@@ -0,0 +1,9 @@
+public class LeapYear {
+    public boolean isLeapYear(int year) {
+        if (year % 4 != 0) {
+            return false;
+        } else if (year % 100 != 0) {
+            return true;
+        } else return year % 400 == 0;
+    }
+}
